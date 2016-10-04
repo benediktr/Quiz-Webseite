9ecb1db7ed1ace0fd140e8df9017cd904e7f69e9
kellerlu <kellerlu1@elektronikschule.de>
2016-10-04 17:19:01 +0200

    Create seite2.php

---
+++ seite2.php
@@ -0,0 +1,15 @@
+<?php
+session_start(); // Ganz wichtig
+ 
+if(!isset($_SESSION['username'])) {
+   die("Bitte erst einloggen"); // Mit die beenden wir den weiteren Scriptablauf   
+}
+ 
+// In $name den Wert der Session speichern
+$name = $_SESSION['username'];
+ 
+// Text ausgeben
+echo "Du heißt immer noch: $name
+<a href=\"logout.php\">Logout</a>";
+?>
+Quelle: http://www.php-einfach.de/php-tutorial/php-sessions/
