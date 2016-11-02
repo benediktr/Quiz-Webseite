d04ff6cfabfb8b0e3f6b976d204cc6113cf3aa84
benediktr <rossbe@elektronikschule.de>
2016-11-02 17:25:03 +0100

    Delete Datenbank_fuellen.php

--- Datenbank_fuellen.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$pdo = new PDO('mysql:host=localhost;dbname=animals', 'root', '');
-	
-	$sql = "INSERT INTO `easy`(`id`, `questions`, `r_answer_1`, `f_answer_2`, `f_answer_3`, `f_answer_4`) VALUES (1, `Was ist ein Haustier?`, `Hase`, `Elephant`, `Krokodil`, `Leopard`)";
-?>
