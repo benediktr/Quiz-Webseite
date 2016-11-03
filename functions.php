c18e9329e829284c0e2ad1468398ed31cc233f87
Lukas <kellerlu1@elektronikschule.de>
2016-11-03 20:45:14 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
 function connect_to_db($db_host, $db_name, $db_user, $db_pass) {
-	$db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);	
-	return $db;
+	$db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+	if($db) {
+		return $db;
+	}
+	else {
+		echo "Connection failed!";
+	}
 }
 ?>
