dcb05e7be438eae6901612fb8006b9a289917aa6
benediktr <rossbe@elektronikschule.de>
2016-11-03 17:44:04 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Startseite</title>
+	</head>
+	<body>
+		<h1>Startseite</h1>
+		<hr>
+		<a href=\login.php\>Login</a>
+		<a href=\register.php\>Registrieren</a>
+	</body>
+</html>
