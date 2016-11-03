ad8e65524a97cbc8d8a0c0421bce881ca1f36bf8
benediktr <rossbe@elektronikschule.de>
2016-11-03 17:43:27 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,6 @@
 <?php
-
-	// Verbindung zur Datenbank
-	function connectToDB($db_host, $db_name, $db_user, $db_pass) {
-		
-		$pdo = new PDO("'mysql:host=$dbhost;dbname=$db_name, $db_user, $db_pass'");
-		
-		if($pdo) {
-			echo 'Verbindung erfolgreich!';
-		} 
-		else {
-			echo 'Verbindung nicht erfolgreich!';
-		}
-	}
-
-	// Zum hinzufügen von Fragen
-	function addQuestions($question, $r_answer1, $f_answer2, $f_answer3, $f_answer4) {
-		$sql = "INSERT INTO `easy`(`questions`, `r_answer_1`, `f_answer_2`, `f_answer_3`, `f_answer_4`) VALUES (`$question`, `$r_answer1`, `$f_answer2`, `$f_answer2`, `$f_answer4`);";
-		mysql_query($sql, $db);
-	}
-	
+function connect_to_db($db_host, $db_name, $db_user, $db_pass) {
+	$db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);	
+	return $db;
+}
 ?>
