0d6cbb35e7f8d6d2d86c58aafb59d18b191d5c47
benediktr <rossbe@elektronikschule.de>
2016-11-03 18:42:02 +0100

    Create password_reset.php

---
+++ password_reset.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<?php
+ session_start();
+ require_once('functions.php');
+ $db = connect_to_db('localhost','user','admin','12345' );
+?>
+
+<?php
+$show_formular = true;
+
+if(isset($_GET['reset'])) {
+	$error_exist = false;
+	$old_password = $_POST['old_password'];
+	$new_password = $_POST['new_password'];
+	$new_password2 = $_POST['new_password2'];
+	$benutzername;
+	
+	if(strlen($old_password) == 0) {
+		echo 'Bitte geben Sie ihr altes Passwort ein!<br>';
+		$error_exist = true;
+	}
+	if($new_password != $new_password2) {
+		echo 'Die Passwörter müssen übereinstimmen<br>';
+		$error_exist = true;
+	}
+	if(!$error_exist) {	
+		$password_hash = password_hash($new_password, PASSWORD_DEFAULT);
+		
+		$statement = $db->prepare("INSERT INTO user_accounts (password) VALUES (:password)");
+		$result = $statement->execute(array('password' => $password_hash));
+		
+		if($result) {		
+			die ('Sie haben erfolgreich einen Account erstellt! <a href="login.php">Weiter zum Login</a>');
+			$show_formular = false;
+		} else {
+			echo 'Fehler bei der Registrierung!<br>';
+		}
+	} 
+}
+
+if($show_formular) {
+
+?>
+
+<form action = "?reset=1" method = "post">
+Altes Passwort:<br>
+<input type = "password" size = "30" maxlength = "30" name = "old_password"><br>
+Passwort wiederholen:<br>
+<input type = "password" size = "30" maxlength = "30" name = "new_password"><br>
+Passwort:<br>
+<input type = "password" size = "30" maxlength = "200" name = "new_password2"><br><br>
+<input type = "submit" value = "Passwort Ändern">
+</form>
+
+<?php
+}
+?>
