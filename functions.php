c22d16dd8e0d6d1ca2ff0e82648f66ccedfd01f5
Lukas <kellerlu1@elektronikschule.de>
2016-11-08 08:45:44 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,4 +8,8 @@ function connect_to_db($db_host, $db_name, $db_user, $db_pass) {
 		echo "Connection failed!";
 	}
 }
+function add_question_to_db($question, $r_answer1, $f_answer2, $f_answer3, $f_answer4, $db_name, $topic) {
+	$add_data = "INSERT INTO $db_name (topic, question, urlname, name, banner, beschreibung) VALUES ('$topic', '$question', '$r_answer1', '$f_answer2', '$f_answer3', '$f_answer4')";
+	// Eintrag an die Datenbank übergeben, mit MySQLi realisieren?
+}
 ?>
