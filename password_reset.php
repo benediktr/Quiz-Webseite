51bcf833aa17d42c34da64071c6b9f406798d6f1
Lukas <kellerlu1@elektronikschule.de>
2016-11-08 08:25:54 +0100

    Update password_reset.php

--- password_reset.php
+++ password_reset.php
@@ -8,45 +8,59 @@
 <?php
 $show_formular = true;
 
-if(isset($_GET['reset'])) {
-	$error_exist = false;
-	$old_password = $_POST['old_password'];
-	$new_password = $_POST['new_password'];
-	$new_password2 = $_POST['new_password2'];
-	$benutzername;
-	
-	if(strlen($old_password) == 0) {
-		echo 'Bitte geben Sie ihr altes Passwort ein!<br>';
-		$error_exist = true;
-	}
-	if($new_password != $new_password2) {
-		echo 'Die Passwörter müssen übereinstimmen<br>';
-		$error_exist = true;
-	}
-	if(!$error_exist) {	
-		$password_hash = password_hash($new_password, PASSWORD_DEFAULT);
-		
-		$statement = $db->prepare("INSERT INTO user_accounts (password) VALUES (:password)");
-		$result = $statement->execute(array('password' => $password_hash));
-		
-		if($result) {		
-			die ('Sie haben erfolgreich ihr Passwort geändert! <a href="index2.php">Weiter zur Startseite</a>');
-			$show_formular = false;
-		} else {
-			echo 'Fehler bei der Änderung ihres Passworts!<br>';
+if(isset($_GET['send']) ) {
+		if(!isset($_POST['old_password']) || empty($_POST['old_password'])) {
+			echo 'Bitte geben Sie ihr altes Passwort ein!';
 		}
-	} 
+		if(!isset($_POST['email']) || empty($_POST['email'])) {
+			echo 'Bitte geben Sie ihre E-Mail ein!';
+		}
+		if (isset($_POST['old_password']) && isset($_POST['email'])) {
+			$statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+			$result = $statement->execute(array('email' => $_POST['email']));
+			$user = $statement->fetch();	
+			if ($user === false) {
+				echo 'Kein Benutzer gefunden!';
+			}
+		}
+		if(isset($_GET['send'])) {
+			$passwort = $_POST['passwort'];
+			$passwort2 = $_POST['passwort2'];
+	
+		if($passwort != $passwort2) {
+			echo 'Bitte identische Passwörter eingeben!';
+		} 
+		else { 
+			$passworthash = password_hash($passwort, PASSWORD_DEFAULT);
+			$statement = $pdo->prepare("UPDATE users SET passwort = :passworthash, passwortcode = NULL, passwortcode_time = NULL WHERE id = :userid");
+			$result = $statement->execute(array('passworthash' => $passworthash, 'userid'=> $userid ));
+		
+			if($result) {
+			die('Dein Passwort wurde erfolgreich geändert!');
+			}
+		}
+	}
 }
+			
 
 if($show_formular) {
-
 ?>
 
 <form action = "?reset=1" method = "post">
+Ihre E-Mail:<br>
+<input type = "email" size = "30" maxlength = "50" name = "email"><br>
 Altes Passwort:<br>
 <input type = "password" size = "30" maxlength = "30" name = "old_password"><br>
 Passwort wiederholen:<br>
 <input type = "password" size = "30" maxlength = "30" name = "new_password"><br>
+Passwort:<br>
+<input type = "password" size = "30" maxlength = "200" name = "new_password2"><br><br>
+<input type = "submit" value = "Passwort Ändern">
+</form>
+
+<?php
+}
+?>
 Passwort:<br>
 <input type = "password" size = "30" maxlength = "200" name = "new_password2"><br><br>
 <input type = "submit" value = "Passwort Ändern">
